5bac71b06723f75a8f4f7bee6ecf81a974dbaee1
Parimba <pedromanoel2007@gmail.com>
2025-10-02 21:53:36 -0300

    Added Mysql database instead of the default of Django DBLite and update requirements.txt

--- src/taskmanager/settings.py
+++ src/taskmanager/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task_manager_db',
+        'USER': 'parimba',
+        'PASSWORD': 'Parimba-G0D',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
